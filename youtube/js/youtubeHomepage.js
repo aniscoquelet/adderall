7a34086ed22a5b6171824a72f957b3112698daa7
aniscoquelet <anis.coquelet@gmail.com>
2020-04-15 20:11:58 +0200

    Blocks Homepage videos + Right column

---
+++ youtube/js/youtubeHomepage.js
@@ -0,0 +1,6 @@
+
+//Ditching this attention seeking recommended div
+let recommended = document.getElementById("secondary");
+let related = document.getElementById('related');
+recommended.remove();
+recommended.style.color = "red";
