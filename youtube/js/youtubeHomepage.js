8a3dfad23d4073c1dcd7e6dfce13c0dea834354f
aniscoquelet <anis.coquelet@gmail.com>
2020-04-16 14:41:17 +0200

    icons added, youtube navigation fixed

--- youtube/js/youtubeHomepage.js
+++ youtube/js/youtubeHomepage.js
@@ -1,6 +1,5 @@
 
-//Ditching this attention seeking recommended div
-let recommended = document.getElementById("secondary");
-let related = document.getElementById('related');
-recommended.remove();
-recommended.style.color = "red";
+
+body.addEventListener("yt-navigate-finish", function(event) {
+    alert('Hello');
+});
